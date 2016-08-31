3ef2b314438e458eda59847d33f36c7d21acc386
fercasti <castilloarevalof@gmail.com>
2016-08-31 17:50:04 -0600

    Create Clase.py

---
+++ Clase.py
@@ -0,0 +1,10 @@
+class automovil:
+   numero_de_ruedas = 0
+   color = “rojo”
+   marca="default"
+
+carro = auto()
+carro.marca="toyota"
+carro.numero_de_ruedas = 4
+carro.color = “negro”
+print "El carro marca “ + carro.marca "tiene“ + carro.numero_de_ruedas + “ ruedas y es de color “ + carro.color
