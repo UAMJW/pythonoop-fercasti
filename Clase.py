c4c253ab7418fa5d19fbe1966610e8507e895e6b
fercasti <castilloarevalof@gmail.com>
2016-08-31 17:56:17 -0600

    Update Clase.py

--- Clase.py
+++ Clase.py
@@ -7,4 +7,4 @@ carro = auto()
 carro.marca="toyota"
 carro.numero_de_ruedas = 4
 carro.color = “negro”
-print "El carro marca “ + carro.marca "tiene“ + carro.numero_de_ruedas + “ ruedas y es de color “ + carro.color
+print "El carro marca “ + carro.marca + "tiene“ + carro.numero_de_ruedas + “ ruedas y es de color “ + carro.color
